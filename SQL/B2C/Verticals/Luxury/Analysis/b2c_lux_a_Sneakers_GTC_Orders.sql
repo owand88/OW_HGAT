18180f1d4230cf9c11363928c0c012427860ac5d
robevans <robevans@ebay.com>
2023-08-01 11:42:17 +0100

    code updated

--- SQL/B2C/Verticals/Luxury/Analysis/b2c_lux_a_Sneakers_GTC_Orders.sql
+++ SQL/B2C/Verticals/Luxury/Analysis/b2c_lux_a_Sneakers_GTC_Orders.sql
@@ -6,33 +6,127 @@
 -- Date Created: 	07/07/2023
 
 
-
+---------------------------------------Avg Listing Duration of live GTC listings-------------------------------
 
 With elig_sneakers
 (
-select item_id
-from access_views.DW_PES_ELGBLT_HIST
-where evltn_end_ts = '2099-12-31 00:00:00'
-and BIZ_PRGRM_NAME = 'PSA_SNEAKER_EBAY_UK' -- UK Sneakers AG program
-and elgbl_yn_ind ='Y' -- eligible items
-and coalesce( auct_end_date, '2099-12-31') >= current_date -- live listing only  
-Group by 1
+select
+	item_id	
+from
+	access_views.DW_PES_ELGBLT_HIST
+where
+	evltn_end_ts = '2099-12-31 00:00:00'
+	and BIZ_PRGRM_NAME = 'PSA_SNEAKER_EBAY_UK'					--UK Sneakers AG program
+	and elgbl_yn_ind ='Y'										--Only eligible items
+	and coalesce( auct_end_date, '2099-12-31') >= current_date	--Only live listings  
+Group by
+	1
 )
 
 
 
+
+-- SELECT
+-- 	lstg_drtn
+-- 	,count(distinct item_id) as ll
+-- FROM
+-- 	(
+	Select
+		lstg.ITEM_ID
+		,case when lstg.RLST_IND = 1 then 'Relisted' else 'New' End as relist_flag
+		,DATEDIFF(CURRENT_DATE, lstg.auct_start_dt) AS lstg_drtn
+	FROM
+		PRS_RESTRICTED_V.SLNG_LSTG_SUPER_FACT lstg
+	INNER JOIN
+		elig_sneakers e
+			on lstg.item_id = e.item_id
+	Where
+		1=1
+		and lstg.SLR_CNTRY_ID = 3		--Only UK sellers
+		and lstg.GTC_IND = 1			--Only include GTC listings
+		and lstg.LSTG_SITE_ID = 3		--Only listings made on the UK site
+		and lstg.LSTG_STATUS_ID = 0		--Only listings that are currently live
+	Group BY
+		1,2,3
+-- 	)
+-- GROUP BY
+-- 	1
+;
+
+
+
+---------------------------------------Avg Listing Duration (time before relisting) of Relisted GTC listings-------------------------------
+
+
+
+drop table if exists relisted_lstgs;
+
+create temp table relisted_lstgs as
+(
+With elig_sneakers
+(
+select
+	item_id	
+from
+	access_views.DW_PES_ELGBLT_HIST
+where
+	evltn_end_ts = '2099-12-31 00:00:00'
+	and BIZ_PRGRM_NAME = 'PSA_SNEAKER_EBAY_UK'					--UK Sneakers AG program
+	and elgbl_yn_ind ='Y'										--Only eligible items
+	and coalesce( auct_end_date, '2099-12-31') >= current_date	--Only live listings  
+Group by
+	1
+)
+
+
 Select
-count(distinct case when GTC_IND = 1 then lstg.ITEM_ID End) as gtc_ag_listings
-,count(distinct lstg.item_id) as total_ag_listings
-,count(distinct case when GTC_IND = 1 then lstg.SLR_ID End) as gtc_ag_sellers
-,count(distinct lstg.SLR_ID) as total_ag_sellers
-,avg(case when GTC_IND = 1 then lstg.START_PRICE_LSTG_CURNCY_AMT End) as gtc_ag_asp
-,avg(lstg.START_PRICE_LSTG_CURNCY_AMT) as total_ag_asp
+	lstg.item_id
+FROM
+	PRS_RESTRICTED_V.SLNG_LSTG_SUPER_FACT lstg
+INNER JOIN
+	elig_sneakers e
+		on lstg.item_id = e.item_id
+INNER JOIN
+	DW_CAL_DT cal
+		on lstg.AUCT_START_DT <= cal.CAL_DT
+		and lstg.AUCT_END_DT >= cal.CAL_DT
+		and cal.retail_year = 2022
+Where
+	1=1
+	and lstg.SLR_CNTRY_ID = 3		--Only UK sellers
+	and lstg.GTC_IND = 1			--Only include GTC listings
+	and lstg.LSTG_SITE_ID = 3		--Only listings made on the UK site
+-- 	and lstg.LSTG_STATUS_ID = 0		--Only listings that are currently live
+	and lstg.RLST_IND = 1
+Group BY
+	1
+)
+;
 
-FROM PRS_RESTRICTED_V.SLNG_LSTG_SUPER_FACT lstg
-INNER JOIN elig_sneakers e
-	on lstg.item_id = e.item_id
 
-Where 1=1
-and lstg.SLR_CNTRY_ID = 3
+select
+	relist_flag
+	,percentile(lstg_drtn, 0.25) AS lower_quartile
+	,percentile(lstg_drtn, 0.50) AS median
+	,percentile(lstg_drtn, 0.75) AS upper_quartile
+FROM
+	(
+	select
+		p.PARENT_ITEM_ID
+		,case when lstg.RLST_IND = 1 then 'Relisted' else 'New' End as relist_flag
+		,DATEDIFF(lstg.AUCT_END_DT, lstg.auct_start_dt) AS lstg_drtn
+	From
+		ACCESS_VIEWS.DW_LSTG_GEN p
+	inner join
+		relisted_lstgs r
+			on p.item_id = r.item_id
+	INNER JOIN
+		PRS_RESTRICTED_V.SLNG_LSTG_SUPER_FACT lstg
+			on p.PARENT_ITEM_ID = lstg.ITEM_ID
 
+	Where 1=1
+		and lstg.GTC_IND = 1
+	Group by 1,2,3
+	)
+GROUP BY
+	1
--- SQL/B2C/Verticals/Luxury/Analysis/b2c_lux_a_Sneakers_eMS_Foreign_Sellers.sql
+++ SQL/B2C/Verticals/Luxury/Analysis/b2c_lux_a_Sneakers_eMS_Foreign_Sellers.sql
@@ -11,35 +11,38 @@
 With elig_sneakers
 (
 select
-	item_id
+	item_id	
 from
 	access_views.DW_PES_ELGBLT_HIST
 where
-	1=1
-	and evltn_end_ts = '2099-12-31 00:00:00'
-	and BIZ_PRGRM_NAME = 'PSA_SNEAKER_EBAY_UK' -- UK Sneakers AG program
-	and elgbl_yn_ind = 'Y' -- eligible items
-	and coalesce(auct_end_date, '2099-12-31') >= current_date -- live listing only  
+	evltn_end_ts = '2099-12-31 00:00:00'
+	and BIZ_PRGRM_NAME = 'PSA_SNEAKER_EBAY_UK'					--UK Sneakers AG program
+	and elgbl_yn_ind ='Y'										--Only eligible items
+	and coalesce( auct_end_date, '2099-12-31') >= current_date	--Only live listings  
 Group by
 	1
 )
 
 
-
 Select
-	c.cntry_desc
-	,lstg.SLR_ID
-	,count(distinct lstg.item_id) as live_listings
+	lstg.SLR_ID
+	,sc.cntry_desc as seller_country
+	,lstg.ITEM_CNTRY_DESC as item_country
+-- 	,avg(DATEDIFF(CURRENT_DATE(), lstg.auct_start_dt)) AS curr_lstg_drtn
+	,count(distinct lstg.item_id) as ll
 FROM
 	PRS_RESTRICTED_V.SLNG_LSTG_SUPER_FACT lstg
-	INNER JOIN elig_sneakers e
+INNER JOIN
+	elig_sneakers e
 		on lstg.item_id = e.item_id
-	LEFT JOIN DW_COUNTRIES c
-		on lstg.SLR_CNTRY_ID = c.cntry_id
+LEFT JOIN
+	DW_COUNTRIES sc 
+		on lstg.SLR_CNTRY_ID = sc.cntry_id
 Where
 	1=1
-	and lstg.LSTG_SITE_ID = 3
-	and lstg.LSTG_STATUS_ID 
+-- 	and lstg.SLR_CNTRY_ID = 3		--Only UK sellers
+	and lstg.GTC_IND = 1			--Only include GTC listings
+	and lstg.LSTG_SITE_ID = 3		--Only listings made on the UK site
+	and lstg.LSTG_STATUS_ID = 0		--Only listings that are currently live
 Group BY
-	1,2
-
+	1,2,3
